4c67b7df5276d355b43b0bad073d7b7ebf9fc3dd
Abhinav Ramesh <dbossgames20@gmail.com>
2024-08-22 10:01:54 -0400

    fix relative imports

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 import os
+import sys
 from flask import Flask, render_template, request, jsonify, Response
 
-# # Add the parent directory to the Python path
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from QuestionGenerator.QuestionGenerator import QuestionGenerator
 from QuestionGenerator.load_syllabus import load_syllabus
 
